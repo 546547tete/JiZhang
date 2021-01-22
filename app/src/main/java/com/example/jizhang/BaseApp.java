16a729ec4a6ce3142f99a667ab5793c602bfdd3e
tirigun <terigun@csii.com.cn>
2021-01-22 13:35:44 +0800

    screen

--- app/src/main/java/com/example/jizhang/BaseApp.java
+++ app/src/main/java/com/example/jizhang/BaseApp.java
@@ -2,13 +2,27 @@ package com.example.jizhang;
 
 import android.app.Application;
 
+import com.example.httplibrary.HttpConstant;
+import com.example.httplibrary.HttpGlobalConfig;
+
 public class BaseApp extends Application {
 
     private static BaseApp getApp;
+    String BASEURL="http://192.168.31.13:8000/";
     @Override
     public void onCreate() {
         super.onCreate();
         getApp=this;
+
+
+        HttpGlobalConfig.getInsance()
+                .setBaseUrl(BASEURL)
+//                .setInterceptors(interceptors)
+                .setTimeout(HttpConstant.TIME_OUT)
+//                .setAppKey(Constans.JPUSHREGISTID,JPushInterface.getRegistrationID(this))
+                .setShowLog(true)
+                .setTimeUnit(HttpConstant.TIME_UNIT)
+                .initReady(this);
     }
 
     public static BaseApp getGetApp() {
--- app/src/main/java/com/example/jizhang/fragment/SettingsFragment.java
+++ app/src/main/java/com/example/jizhang/fragment/SettingsFragment.java
@@ -3,10 +3,16 @@ package com.example.jizhang.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.example.jizhang.R;
 
@@ -14,6 +20,10 @@ import com.example.jizhang.R;
  * A simple {@link Fragment} subclass.
  */
 public class SettingsFragment extends Fragment {
+
+    private RecyclerView rcy;
+    private EditText et_add;
+    private Button bt_add;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -24,6 +34,19 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view) {
+        et_add = view.findViewById(R.id.et_add_settings);
+        bt_add = view.findViewById(R.id.bt_add_settings);
+        rcy = view.findViewById(R.id.rcy_settings);
+
+        rcy.setLayoutManager(new LinearLayoutManager(getContext()));
+        rcy.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayout.VERTICAL));
+
+
     }
 }
