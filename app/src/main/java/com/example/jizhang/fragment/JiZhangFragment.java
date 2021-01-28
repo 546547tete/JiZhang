e3a95297555278b7b77a03efcebbae2babef1482
chaolemen-github <2863346378@qq.com>
2021-01-28 11:26:57 +0800

    类别下拉框

--- app/src/main/java/com/example/jizhang/fragment/JiZhangFragment.java
+++ app/src/main/java/com/example/jizhang/fragment/JiZhangFragment.java
@@ -8,9 +8,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,13 +23,14 @@ import androidx.fragment.app.Fragment;
 
 import com.example.httplibrary.utils.LogUtils;
 import com.example.jizhang.R;
-import com.example.jizhang.bean.AddEntryBean;
+import com.example.jizhang.bean.CategoriesBean;
 import com.example.jizhang.bean.DataBean;
 import com.example.jizhang.utils.ApiService;
 import com.example.jizhang.utils.ContextUtils;
 import com.example.jizhang.utils.RetrofitUtils;
-import com.example.jizhang.utils.UUIDUtils;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
@@ -55,6 +59,11 @@ public class JiZhangFragment extends Fragment implements View.OnClickListener {
     private String time;
     private Calendar calendar;
     private static final String TAG = "JiZhangFragment";
+    private TextView mSpinnerText;
+    private Spinner mSpinner01;
+    private ArrayAdapter<String> adapter;
+    private List<CategoriesBean> list;
+    private String[] arr;
 
 
     public JiZhangFragment() {
@@ -72,9 +81,9 @@ public class JiZhangFragment extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         View inflate = inflater.inflate(R.layout.fragment_ji_zhang, container, false);
         initView(inflate);
+        initData();
         return inflate;
     }
-
 
 
     private void initView(@NonNull final View itemView) {
@@ -84,6 +93,8 @@ public class JiZhangFragment extends Fragment implements View.OnClickListener {
         mShuomingJizhangEt = (EditText) itemView.findViewById(R.id.et_shuoming_jizhang);
         mOkJizhangBt = (Button) itemView.findViewById(R.id.bt_ok_jizhang);
         mOkJizhangBt.setOnClickListener(this);
+        mSpinnerText = (TextView) itemView.findViewById(R.id.spinnerText);
+        mSpinner01 = (Spinner) itemView.findViewById(R.id.Spinner01);
 
         calendar = Calendar.getInstance();
 
@@ -94,6 +105,21 @@ public class JiZhangFragment extends Fragment implements View.OnClickListener {
             }
         });
 
+
+
+    }
+
+    //使用数组形式操作
+    class SpinnerSelectedListener implements AdapterView.OnItemSelectedListener {
+
+        public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+                                   long arg3) {
+            mSpinnerText.setText(arr[arg2]);
+            LogUtils.e("99999999999999999999999999");
+        }
+
+        public void onNothingSelected(AdapterView<?> arg0) {
+        }
     }
 
     @Override
@@ -105,8 +131,9 @@ public class JiZhangFragment extends Fragment implements View.OnClickListener {
                 String shuoming = mShuomingJizhangEt.getText().toString().trim();
                 String money = mMoneyJizhangEt.getText().toString().trim();
                 String trim = tv_time_jizhang.getText().toString().trim();
+                String spinnerText = mSpinnerText.getText().toString().trim();
 
-                if (TextUtils.isEmpty(trim)){
+                if (TextUtils.isEmpty(trim)) {
                     return;
                 }
                 String[] split = trim.split("-");
@@ -120,16 +147,16 @@ public class JiZhangFragment extends Fragment implements View.OnClickListener {
 
                 int parseInt = Integer.parseInt(money);
 
-                if (!TextUtils.isEmpty(trim)&&!TextUtils.isEmpty(leibie)&&!TextUtils.isEmpty(money)&&!TextUtils.isEmpty(shuoming)){
+                if (!TextUtils.isEmpty(spinnerText) && !TextUtils.isEmpty(trim) && !TextUtils.isEmpty(money) && !TextUtils.isEmpty(shuoming)) {
 //                    initData(y,m,d,leibie,shuoming,parseInt);
-                    initDateNew(y,m,d,leibie,shuoming,parseInt);
-                }else if (TextUtils.isEmpty(leibie)){
+                    initDateNew(y, m, d, spinnerText, shuoming, parseInt);
+                } else if (TextUtils.isEmpty(spinnerText)) {
                     Toast.makeText(getContext(), "请输入类别", Toast.LENGTH_SHORT).show();
-                }else if (TextUtils.isEmpty(shuoming)){
+                } else if (TextUtils.isEmpty(shuoming)) {
                     Toast.makeText(getContext(), "请添加备注", Toast.LENGTH_SHORT).show();
-                }else if (TextUtils.isEmpty(money)){
+                } else if (TextUtils.isEmpty(money)) {
                     Toast.makeText(getContext(), "请输入金额", Toast.LENGTH_SHORT).show();
-                }else if (TextUtils.isEmpty(trim)){
+                } else if (TextUtils.isEmpty(trim)) {
                     Toast.makeText(getContext(), "请输入日期", Toast.LENGTH_SHORT).show();
                 }
                 break;
@@ -138,36 +165,48 @@ public class JiZhangFragment extends Fragment implements View.OnClickListener {
         }
     }
 
-    private void initData(int y, int m, int d, String leibie, String shuoming, int money) {
+    private void initData() {
         HashMap<String, Object> map = new HashMap<>();
-        map.put("hardware_id",ContextUtils.UUID_JIZHANG);
-        map.put("year",y);
-        map.put("month",m);
-        map.put("day",d);
-        map.put("category",leibie);
-        map.put("description",shuoming);
-        map.put("amount",money);
-        Log.e("", "initData: " + ContextUtils.UUID_JIZHANG);
+        map.put("hardware_id", ContextUtils.UUID_JIZHANG);
         RetrofitUtils.getInstance()
-                .getAddEntry(map)
+                .getCategories("categories/" + ContextUtils.UUID_JIZHANG + "/")
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<DataBean>() {
+                .subscribe(new Observer<List<CategoriesBean>>() {
                     @Override
                     public void onSubscribe(Disposable d) {
 
                     }
 
                     @Override
-                    public void onNext(DataBean addEntryBean) {
-                        if (addEntryBean!=null){
-                            Toast.makeText(getContext(), addEntryBean.getError_msg(), Toast.LENGTH_SHORT).show();
+                    public void onNext(List<CategoriesBean> categoriesPieBeans) {
+                        //将数据添加到集合中
+
+                        arr = new String[categoriesPieBeans.size()];
+                        for (int i = 0; i < categoriesPieBeans.size(); i++) {
+                            arr[i] = categoriesPieBeans.get(i).getCategory_name();
                         }
+
+
+                        //将可选内容与ArrayAdapter连接起来
+                        adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,arr);
+
+                        //设置下拉列表的风格
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                        //将adapter 添加到spinner中
+                        mSpinner01.setAdapter(adapter);
+
+                        //添加事件Spinner事件监听
+                        mSpinner01.setOnItemSelectedListener(new SpinnerSelectedListener());
+
+                        //设置默认值
+                        mSpinner01.setVisibility(View.VISIBLE);
                     }
 
                     @Override
                     public void onError(Throwable e) {
-                        Log.e("TAG = AddEntry", e.getMessage());
+                        LogUtils.e(e.getMessage());
                     }
 
                     @Override
@@ -206,12 +245,12 @@ public class JiZhangFragment extends Fragment implements View.OnClickListener {
                 , calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 
-    private void initDateNew(int y, int m, int d,String leibie, String shuoming, int money) {
+    private void initDateNew(int y, int m, int d, String leibie, String shuoming, int money) {
         HashMap<String, Object> map = new HashMap<>();
         map.put("hardware_id", ContextUtils.UUID_JIZHANG);
-        map.put("year",y);
-        map.put("month",m);
-        map.put("day",d);
+        map.put("year", y);
+        map.put("month", m);
+        map.put("day", d);
         map.put("category", leibie);
         map.put("description", shuoming);
         map.put("amount", money);
@@ -232,14 +271,14 @@ public class JiZhangFragment extends Fragment implements View.OnClickListener {
 
                     @Override
                     public void onNext(DataBean dataBean) {
-                        if (dataBean.getError_code()==0){
+                        if (dataBean.getError_code() == 0) {
                             Toast.makeText(getContext(), "添加成功", Toast.LENGTH_SHORT).show();
                         }
                     }
 
                     @Override
                     public void onError(Throwable e) {
-                        Log.e("TAG",e.getMessage());
+                        Log.e("TAG", e.getMessage());
                     }
 
                     @Override
